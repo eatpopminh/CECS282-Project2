8610db326dd529e0c7290acf54d76c33f07bfa41
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2019-10-13 13:44:59 -0700

    Got TIme to be January 1, 1970

--- Time.cpp
+++ Time.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 Time :: Time()
 {
-	secs = time(0);
+	secs = 0;
 	//cout<<ctime(&secs)<<endl;
 }
 Time :: Time(time_t t)
@@ -22,16 +22,17 @@ Time :: Time(const Time& t)
 }
 string Time :: toString() const //return time in the following format Www Mmm dd hh:mm:ss yyyy
 {
-	struct tm *myString = localtime(&secs);
-	return asctime(myString);
+	//struct tm *myString = localtime(&secs);
+	return asctime(gmtime(&secs));
 	
 	
 }		
 string Time :: getMMDDYYYY() const		//return time in the following format MM/DD/YYYY
 {
-	struct tm *myString = localtime(&secs);
+	//struct tm *myString = localtime(&secs);
+	//return (to_string(myString -> tm_mon+1)+"/"+to_string(myString -> tm_mday)+"/"+to_string(myString -> tm_year + 1900));
+	struct tm *myString = gmtime(&secs);
 	return (to_string(myString -> tm_mon+1)+"/"+to_string(myString -> tm_mday)+"/"+to_string(myString -> tm_year + 1900));
-	
 }
 string Time :: getYear() const 			//return year of time.
 {
@@ -45,18 +46,34 @@ int Time :: compareTime(const Time& t)
 int main()
 {
 	cout<<"HELLO"<<endl;
-	Time t;
+	Time t = 0;
 	cout<<t.toString()<<endl;
 	cout<<t.getMMDDYYYY()<<endl;
-	cout<<t.getYear()<<endl;
+	//cout<<t.getYear()<<endl;
 	
 	
-	time_t sec = time(0);
-	time_t sec2;
-	time(&sec2);
-	struct tm *myTime = localtime(&sec2);
+	//time_t secs = 0;
+	//cout<<asctime(gmtime(&secs));
+//	time(&sec);
+//	cout<<ctime(&sec)<<endl;
+//	struct tm *myString = {0};
+//	myString->tm_hour = 0;
+//	myString->tm_mday = 0;
+//	myString->tm_min = 0;
+//	myString->tm_mon = 1;
+//	myString->tm_sec = 0;
+//	myString->tm_year = 100;
+//	cout<<asctime(myString)<<endl;
+//	//cout<<(to_string(myString -> tm_mon-(myString -> tm_mon))+"/"+to_string(myString -> tm_mday-(myString->tm_mday))+"/"+to_string(myString -> tm_year+1781))<<endl;;
+	
+
+
+//	time_t sec2 = 1000;
+//	time(&sec2);
+//	cout<<ctime (&sec2)<<endl;
+	//struct tm *myTime = localtime(&sec2);
 	//string mystring = to_string(myTime->tm_mon+1);
-	string mystring = asctime(myTime);
-	cout<<mystring<<endl;
+	//string mystring = asctime(myTime);
+	//cout<<mystring<<endl;
 	
 }
